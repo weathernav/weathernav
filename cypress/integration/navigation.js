3af73cc18e2d9a20e737a970c080f415d7ea0a51
Ezra Chu <ezra.j.chu@gmail.com>
2018-12-26 17:53:41 -0500

    prettier tests

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -1,35 +1,31 @@
 describe('Navigation', function() {
-  const searchLocations= function(locations){
-    cy.visit('http://localhost:3000')
-    cy.get('input').each((el, i)=>{
+  const searchLocations = function(locations) {
+    cy.visit('http://localhost:3000');
+    cy.get('input').each((el, i) => {
       cy.wrap(el)
         .type(locations[i])
-        .type('{downarrow}{enter}', {delay: 500})
-    })
-  }
-  context('if route is long enough for weather', function(){
-    before(()=> {
-      const locations = [
-        'Tucker, GA',
-        'Austin, TX'
-      ]
-      searchLocations(locations)
-    })
-    it('should render weather infoboxes', ()=> {
-      cy.get('.infoBox').its('length').should('be.gt', 0)
-    })
-  })
+        .type('{downarrow}{enter}', { delay: 500 });
+    });
+  };
+  context('if route is long enough for weather', function() {
+    before(() => {
+      const locations = ['Tucker, GA', 'Austin, TX'];
+      searchLocations(locations);
+    });
+    it('should render weather infoboxes', () => {
+      cy.get('.infoBox')
+        .its('length')
+        .should('be.gt', 0);
+    });
+  });
 
-  context('if route is not long enough for weather', function(){
-    before(()=> {
-      const locations = [
-        'Tucker, GA',
-        'Atlanta, GA'
-      ]
-      searchLocations(locations)
-    })
-    it('should not render weather infoboxes', ()=> {
-      cy.get('.infoBox').should('have.length', 0)
-    })
-  })
-})
+  context('if route is not long enough for weather', function() {
+    before(() => {
+      const locations = ['Tucker, GA', 'Atlanta, GA'];
+      searchLocations(locations);
+    });
+    it('should not render weather infoboxes', () => {
+      cy.get('.infoBox').should('have.length', 0);
+    });
+  });
+});
